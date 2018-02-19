6c4fd82ca8261445ced298b28d511bae2827ce87
Jake Neels <nizlmmk@gmail.com>
2018-02-18 19:37:39 -0500

    added HistMarketData service, HistDayData model for accessing included dataset HistData. modeled KeyStatsData from IEX api sorry about the 500mb ; )

--- StockMarket/Playground/Program.cs
+++ StockMarket/Playground/Program.cs
@@ -1,6 +1,8 @@
 ﻿using StockService;
+using StockService.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +13,26 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            var stockSvc = new IEXStockService();
-            var stock = stockSvc.GetCompanyData("aapl");
-            Console.WriteLine(stock.CompanyName);
+         
+        var hist = new HistMarketData();
+
+           var stockSvc = new IEXStockService();
+            // var stock = stockSvc.GetTopsMarketDataHist("aapl",20171015 ); //yyyymmdd
+
+            //stockSvc.GetCompanyData("aapl");
+
+            hist.ParseHistMarketDataTXT("aapl");
+            //hist.ParseHistMarketDataTXT("goog");
+
+           // hist.ParseHistMarketDataTXT("a");
+
+            hist.PrintMarketData();
+            // Console.WriteLine(Path.Combine(AppDomain.CurrentDomain.BaseDirectory));
+            // Console.WriteLine(Directory.GetParent(Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory).ToString()).ToString()).ToString());
+
+
+            //Console.WriteLine($" aks price {stock.AskPrice} bid price  {stock.BidPrice}   bid size{stock.BidSize}  ask size {stock.AskSize}");
             Console.ReadKey();
         }
-
-
     }
 }
--- StockMarket/StockService/Services/IEXStockService.cs
+++ StockMarket/StockService/Services/IEXStockService.cs
@@ -25,6 +25,7 @@ namespace StockService
                 {
                     var jsonStr = sr.ReadToEnd();
                     stocks = JsonConvert.DeserializeObject<List<TopsMarketData>>(jsonStr);
+                    Console.WriteLine(jsonStr);
                 }
             }
             catch (Exception ex)
@@ -69,6 +70,8 @@ namespace StockService
                 {
                     var jsonStr = sr.ReadToEnd();
                     stocks = JsonConvert.DeserializeObject<List<TopsMarketData>>(jsonStr);
+                    Console.WriteLine(jsonStr);
+
                 }
             }
             catch (Exception ex)
@@ -92,6 +95,8 @@ namespace StockService
                 {
                     var jsonStr = sr.ReadToEnd();
                     stock = JsonConvert.DeserializeObject<CompanyData>(jsonStr);
+                    Console.WriteLine(jsonStr);
+
                 }
             }
             catch (Exception ex)
@@ -101,5 +106,130 @@ namespace StockService
 
             return stock;
         }
+
+
+        public KeyStatsData GetKeyStats(String symbol) // Needs conversion for KeyStatsDAta
+        {
+            var symbols = new List<String>();
+            symbols.Add(symbol);
+            var stocks = GetKeyStats(symbols);
+            return stocks.ElementAt(0);
+        }
+
+        public List<KeyStatsData> GetKeyStats(List<String> symbols)  // Needs conversion for KeyStatsDAta
+        {
+            List<KeyStatsData> stocks = null;
+            string uri = "";
+
+            try
+            {
+                //https://api.iextrading.com/1.0/stock/aapl/stats
+                bool isFirstTime = true;
+                foreach (var symbol in symbols)
+                {
+                    uri = "https://api.iextrading.com/1.0/stock/{symbol}/stats";
+
+
+                    uri += symbol;
+                }
+                var req = WebRequest.Create(uri);
+                var stream = req.GetResponse().GetResponseStream();
+                using (StreamReader sr = new StreamReader(stream))
+                {
+                    var jsonStr = sr.ReadToEnd();
+                    stocks = JsonConvert.DeserializeObject<List<KeyStatsData>>(jsonStr);
+                    Console.WriteLine(jsonStr);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return stocks;
+        }
+
+
+
+
+
+
+
+       
+        
+        
+        
+        
+        //Depricated??
+
+        //public List<TopsMarketData> GetTopsMarketDataHist()
+        //{
+        //    List<TopsMarketData> stocks = null;
+
+        //    try
+        //    {
+        //        var req = WebRequest.Create("https://api.iextrading.com/1.0/hist");
+        //        var stream = req.GetResponse().GetResponseStream();
+        //        using (StreamReader sr = new StreamReader(stream))
+        //        {
+        //            var jsonStr = sr.ReadToEnd();
+        //            stocks = JsonConvert.DeserializeObject<List<TopsMarketData>>(jsonStr);
+        //            Console.WriteLine(jsonStr);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //    }
+
+        //    return stocks;
+        //}
+
+        //public TopsMarketData GetTopsMarketDataHist(String symbol, int date)
+        //{
+        //    var symbols = new List<String>();
+        //    symbols.Add(symbol);
+        //    var stocks = GetTopsMarketDataHist(symbols, date);
+        //    return stocks.ElementAt(0);
+        //}
+
+        //public List<TopsMarketData> GetTopsMarketDataHist(List<String> symbols , int date)
+        //{
+        //    List<TopsMarketData> stocks = null;
+
+        //    try
+        //    {
+        //        String uri = "https://api.iextrading.com/1.0/hist?date={date}symbols=";
+        //        bool isFirstTime = true;
+        //        foreach (var symbol in symbols)
+        //        {
+        //            if (!isFirstTime)
+        //            {
+        //                uri += ",";
+        //            }
+        //            else
+        //            {
+        //                isFirstTime = false;
+        //            }
+        //            uri += symbol;
+        //        }
+        //        var req = WebRequest.Create(uri);
+        //        var stream = req.GetResponse().GetResponseStream();
+        //        using (StreamReader sr = new StreamReader(stream))
+        //        {
+        //            var jsonStr = sr.ReadToEnd();
+        //            stocks = JsonConvert.DeserializeObject<List<TopsMarketData>>(jsonStr);
+        //            Console.WriteLine(jsonStr);
+
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //    }
+
+        //    return stocks;
+        //}
     }
 }
