cca6e31fdefcb56964fa60a1c09d71aed691984b
jakeneels <nizlmmk@gmail.com>
2018-03-07 10:32:55 -0500

    ss

---
+++ StockMarket/StockAlgorithms/Models/ClusterCandle.cs
@@ -0,0 +1,27 @@
+﻿using StockAnalyzer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAlgorithms.Models
+{
+    class ClusterCandle : Stock
+    {
+        List<Stock> _cluster = new List<Stock>();
+
+        public List<Stock> Cluster
+        {
+            get
+            {
+                return _cluster;
+            }
+        }
+
+        public ClusterCandle(DateTime startDate)
+        {
+
+        }
+    }
+}
