e9571ecee645b92ce2e65d76d46b7795b34da154
jakeneels <nizlmmk@gmail.com>
2018-03-07 12:47:44 -0500

    CRUD skeleton

--- StockMarket/StockAlgorithms/Services/DatabaseService.cs
+++ StockMarket/StockAlgorithms/Services/DatabaseService.cs
@@ -1,4 +1,5 @@
-﻿using StockAnalyzer;
+﻿using StockAlgorithms.Models;
+using StockAnalyzer;
 using StockAnalyzer.Models;
 using System;
 using System.Collections.Generic;
@@ -11,43 +12,162 @@ namespace StockAlgorithms.Services
 {
     public class DatabaseService : IDatabaseService
     {
-        private const string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=ProjectDB;Integrated Security=true";
+        private const string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=StockMarket;Integrated Security=true";
 
         public List<Stock> GetStockData(string symbol, int numDaysBack)
         {
             throw new NotImplementedException();
         }
 
-
         public List<Stock> GetStockData(List<Stock> list, string symbol, int numDaysBack)
         {
             throw new NotImplementedException();
         }
 
-    public void AddTicker()
-    {
-        try
+        // start Ticker table methods
+
+        public void CreateTicker(Ticker ticker)
         {
-            using (SqlConnection conn = new SqlConnection(connectionString))
-            {
-                conn.Open();
+            //try
+            //{
+            //    using (SqlConnection conn = new SqlConnection(connectionString))
+            //    {
+            //        conn.Open();
 
-                SqlCommand cmd = new SqlCommand(SQL_AddCity, conn);
-                cmd.Parameters.AddWithValue("@id", city.CityId);
-                cmd.Parameters.AddWithValue("@name", city.Name);
-                cmd.Parameters.AddWithValue("@countrycode", city.CountryCode);
-                cmd.Parameters.AddWithValue("@district", city.District);
-                cmd.Parameters.AddWithValue("@population", city.Population);
+            //        SqlCommand cmd = new SqlCommand(SQL_AddCity, conn);
+            //        cmd.Parameters.AddWithValue("@id", city.CityId);
+            //        cmd.Parameters.AddWithValue("@name", city.Name);
+            //        cmd.Parameters.AddWithValue("@countrycode", city.CountryCode);
+            //        cmd.Parameters.AddWithValue("@district", city.District);
+            //        cmd.Parameters.AddWithValue("@population", city.Population);
 
-                cmd.ExecuteReader();
-            }
+            //        cmd.ExecuteReader();
+            //    }
+            //}
+            //catch (SqlException ex)
+            //{
+            //    throw;
+            //}
         }
-        catch (SqlException ex)
+
+        public void ListTickers()
         {
-            throw;
+            throw new NotImplementedException();
         }
-    }
 
+        public void ReadTicker(Ticker ticker)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void RemoveTicker(Ticker ticker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTicker(Ticker ticker)
+        {
+            throw new NotImplementedException();
+        }
+
+        // end Ticker table methods
+        //Start Stock table methods
+
+        public void CreateStock(Stock stock)
+        {
+            //try
+            //{
+            //    using (SqlConnection conn = new SqlConnection(connectionString))
+            //    {
+            //        conn.Open();
+
+            //        SqlCommand cmd = new SqlCommand(SQL_AddCity, conn);
+            //        cmd.Parameters.AddWithValue("@id", city.CityId);
+            //        cmd.Parameters.AddWithValue("@name", city.Name);
+            //        cmd.Parameters.AddWithValue("@countrycode", city.CountryCode);
+            //        cmd.Parameters.AddWithValue("@district", city.District);
+            //        cmd.Parameters.AddWithValue("@population", city.Population);
+
+            //        cmd.ExecuteReader();
+            //    }
+            //}
+            //catch (SqlException ex)
+            //{
+            //    throw;
+            //}
+
+            throw new NotImplementedException();
+        }
+
+        public void ListStocks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadStock(Stock stock)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveStock(Stock stock)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStock(Stock stock)
+        {
+            throw new NotImplementedException();
+        }
+
+        //end stock table methods
+        //Start Sector table methods
+
+        public void CreateSector(Sector sector)
+        {
+            //try
+            //{
+            //    using (SqlConnection conn = new SqlConnection(connectionString))
+            //    {
+            //        conn.Open();
+
+            //        SqlCommand cmd = new SqlCommand(SQL_AddCity, conn);
+            //        cmd.Parameters.AddWithValue("@id", city.CityId);
+            //        cmd.Parameters.AddWithValue("@name", city.Name);
+            //        cmd.Parameters.AddWithValue("@countrycode", city.CountryCode);
+            //        cmd.Parameters.AddWithValue("@district", city.District);
+            //        cmd.Parameters.AddWithValue("@population", city.Population);
+
+            //        cmd.ExecuteReader();
+            //    }
+            //}
+            //catch (SqlException ex)
+            //{
+            //    throw;
+            //}
+
+            throw new NotImplementedException();
+        }
+
+        public void ListSectors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadSector(Sector sector)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSector(Sector sector)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSector(Sector sector)
+        {
+            throw new NotImplementedException();
+        }
+
+        //end Stock table methods
     }
 }
---
+++ StockMarket/StockService/create.sql
@@ -0,0 +1,49 @@
+BEGIN TRANSACTION
+	CREATE DATABASE StockMarket
+
+	CREATE TABLE Ticker
+	(
+		Id int Identity(1,1),
+		Symbol VARCHAR(7) NOT NULL,
+		CompanyName VARCHAR(75) NOT NULL,
+		Exchange VARCHAR(40) NOT NULL,
+		Industry VARCHAR(30) NOT NULL,
+		Website VARCHAR(100) NOT NULL,
+		Description VARCHAR(200) NOT NULL,
+
+		CONSTRAINT pk_ticker PRIMARY KEY (Id)
+	)
+
+	CREATE TABLE Stock
+	(
+		Id int Identity(1,1),
+		TickerId int NOT NULL,
+		Date Date Default GetDate NOT NULL,
+		OpenPrice float NOT NULL,
+		ClosePrice float NOT NULL,
+		High float NOT NULL,
+		Low float NOT NULL,
+		Volume Int NOT NULL,
+
+		CONSTRAINT pk_stock PRIMARY KEY (Id),
+		CONSTRAINT fk_stock_ticker FOREIGN KEY (TickerId) REFERENCES Ticker (Id),
+	)
+
+	CREATE TABLE KeyStats
+	(
+		Id int Identity(1,1),
+		TickerId int NOT NULL,
+
+		CONSTRAINT pk_KeyStat PRIMARY KEY (Id),
+		CONSTRAINT fk_KeyStat_ticker FOREIGN KEY (TickerId) REFERENCES Ticker (Id),
+	)
+
+	CREATE TABLE Financials
+	(
+		Id int Identity(1,1),
+		TickerId int NOT NULL,
+
+		CONSTRAINT pk_financials PRIMARY KEY (Id),
+		CONSTRAINT fk_financials_ticker FOREIGN KEY (TickerId) REFERENCES Ticker (Id),
+	)
+COMMIT TRANSACTION
